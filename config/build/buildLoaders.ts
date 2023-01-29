f5e842c2bd403a178baad73a2ee73da9f319823b
Danil <vetlzdanilvv@gmail.com>
2023-01-29 18:39:40 +0500

    feat: added react n scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+    }
+
     return [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
+        typescriptLoader,
+        cssLoader
     ]
 }
