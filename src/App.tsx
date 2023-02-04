4dfaf1ddef484f233eb8ebb1248e756d9cc5a412
Danil <vetlzdanilvv@gmail.com>
2023-02-04 13:38:49 +0500

    feat: added themes n helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
-import './index.scss';
+import React, {Suspense, useContext} from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import './styles/index.scss';
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
-            <Counter />
-            dsadsadsad
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>change theme</button>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={"Loading..."}>
+                <Routes>
+                    <Route
+                        path={'/'}
+                        element={<MainPageAsync />}
+                    />
+                    <Route
+                        path={'/about'}
+                        element={<AboutPageAsync />}
+                    />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
