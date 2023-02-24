4bd3f064215e8600dc6a25f9adb3e45605c7a605
Danil <vetlzdanilvv@gmail.com>
2023-02-24 14:41:48 +0500

    feat: added first redux slice

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import cls from './NotFoundPage.module.scss';
 
-export const NotFoundPage = () => {
-    const { t } = useTranslation();
+interface NotFoundPageProps {
+    className?: string;
+}
 
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+    const { t } = useTranslation();
     return (
-        <div className={classNames(cls.NotFoundPage)}>
+        <div className={classNames(cls.NotFoundPage, {}, [className])}>
             {t('Страница не найдена')}
         </div>
     );
